fd1226e8fdec2ffedd19dfa358743810b8e9c2f5
Svetoslav Petkov <svetoslav@alkem.io>
2024-10-18 17:01:39 +0300

    Excalidraw update (#35)
    * accommodate new excalidraw version
    
    * version bump
    
    * npm audit fix
    
    * wip
    
    * typos
    
    * removed todo; added doc

--- src/excalidraw-backend/utils/array.to.map.by.ts
+++
@@ -1,9 +0,0 @@
-export const arrayToMapBy = <T extends Record<string, any>>(
-  items: readonly T[],
-  key: keyof T,
-): Map<string, T> => {
-  return items.reduce((acc: Map<string, T>, element) => {
-    acc.set(element[key], element);
-    return acc;
-  }, new Map<string, T>());
-};
