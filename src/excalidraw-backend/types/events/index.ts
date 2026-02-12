ab83f4143ee81649beee533a75fd16bc9cf89d6a
Carlos Cano <carlos@alkem.io>
2026-02-12 20:55:49 +0100

    countdown timers

--- src/excalidraw-backend/types/events/index.ts
+++ src/excalidraw-backend/types/events/index.ts
@@ -2,3 +2,4 @@ export * from './idle.state.payload';
 export * from './server.broadcast.payload';
 export * from './scene.init.payload';
 export * from './emoji.reaction.payload';
+export * from './countdown.timer.payload';
